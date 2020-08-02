0e0c2f13a7476097722350b4ccd9c2f8b03d48ff
Lucas Fernando Amorim <lf.amorim@yahoo.com.br>
2020-08-02 00:00:40 -0300

    feature: versão e coisas mais

--- lib-generate.js
+++ lib-generate.js
@@ -5,4 +5,4 @@ const buffer = 'module.exports = function jurischain() {' +
     fs.readFileSync(require.resolve('jurischain'))
     + '};'
 
-fs.writeFileSync('dist/jurischain-runner.js', js(buffer))
+fs.writeFileSync('./lib/jurischain-runner.js', js(buffer))
